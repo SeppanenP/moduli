59521a64fe14fbe73c358c1a24b2aa24b367ee23
SeppanenP <32223742+SeppanenP@users.noreply.github.com>
2018-12-03 21:22:57 +0200

    Create index.php

---
+++ srv/salt/lamp/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>OmaSivu</title>
+<meta charset=”UTF-8″>
+</head>
+<body>
+
+<h1>Terve Maailma!!!</h1>
+<br>
+<!-- Alla testi toimiiko PHP -->
+2 + 2 = 
+ <?php
+	 print(2+2);
+ ?>
+ 
+</body>
+</html>
