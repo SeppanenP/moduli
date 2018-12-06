e010ca111fe27fc5e4d7386be6bb9cb6b33c5f00
SeppanenP <32223742+SeppanenP@users.noreply.github.com>
2018-12-06 12:31:45 +0000

    Create mariasettings.sql

---
+++ srv/salt/setmariadb/mariasettings.sql
@@ -0,0 +1,9 @@
+# Create Database
+CREATE DATABASE products;
+USE products;
+CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1024), price FLOAT);
+INSERT INTO products (name, price) VALUES ("Product 1" , 99.9);
+
+# Create user "test" and allow database "products"
+GRANT ALL ON products.* TO test@localhost IDENTIFIED BY 'really_good_password_here';
+
