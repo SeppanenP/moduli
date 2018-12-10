bebb22d5a1b3c32b4025b92468d36a25916b8654
SeppanenP <32223742+SeppanenP@users.noreply.github.com>
2018-12-10 18:27:47 +0200

    Update mariasettings.sql

--- srv/salt/setmariadb/mariasettings.sql
+++ srv/salt/setmariadb/mariasettings.sql
@@ -5,6 +5,6 @@ CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1024), pr
 INSERT INTO products (name, price) VALUES ("Product 1" , 99.9);
 INSERT INTO products (name, price) VALUES ("Product 2" , 55.5);
 
-# Create user "test" and allow database "products"
-GRANT ALL ON products.* TO test@localhost IDENTIFIED BY 'really_good_password_here';
+# Create user "newuser" and allow database "products"
+GRANT ALL ON products.* TO newuser@localhost IDENTIFIED BY 'really_good_password_here';
 
