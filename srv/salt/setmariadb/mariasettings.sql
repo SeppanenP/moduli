f0c51c136cb493a98a0f6dab521c72cf8b2fcb70
SeppanenP <32223742+SeppanenP@users.noreply.github.com>
2018-12-10 19:17:59 +0200

    Update mariasettings.sql

--- srv/salt/setmariadb/mariasettings.sql
+++ srv/salt/setmariadb/mariasettings.sql
@@ -6,5 +6,5 @@ INSERT INTO products (name, price) VALUES ("Product 1" , 99.9);
 INSERT INTO products (name, price) VALUES ("Product 2" , 55.5);
 
 # Create user "test" and allow database "products"
-GRANT ALL ON products.* TO test@localhost IDENTIFIED BY 'really_good_password_here'
+GRANT ALL ON products.* TO test@localhost IDENTIFIED BY 'really_good_password_here';
 
